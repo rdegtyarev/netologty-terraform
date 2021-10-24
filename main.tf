00eb8216f896d22548be00ec8a4d27708a5795e1
Roman Degtyarev <degtyarev.roman@gmail.com>
2021-10-24 21:01:06 +0300

    add terraform

---
+++ main.tf
@@ -0,0 +1,33 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
+
+output "aws_region" {
+  value = data.aws_region.current.name
+}
