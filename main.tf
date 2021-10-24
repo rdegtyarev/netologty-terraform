b1197e547c03e98e275b7accdcc7585cdc31da83
Roman Degtyarev <degtyarev.roman@gmail.com>
2021-10-24 21:14:13 +0300

    edit main.tf

--- main.tf
+++ main.tf
@@ -3,25 +3,9 @@ provider "aws" {
   region = "us-east-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami           = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
